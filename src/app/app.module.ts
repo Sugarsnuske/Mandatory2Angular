c6718d70ce0c63166d586ef9ff4511e2e4993aac
Jeff Yahin Jørgensen (FVST) <B029478@PROD.SITAD.DK>
2017-05-10 16:28:59 +0200

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { InternshipListComponent } from './internship-list/internship-list.component';
 import { InternshipDetailsComponent } from './internship-list/internship-details/internship-details.component';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 /* Application-wide when in the app.module */
 import { InternshipService } from './internship-list/internship.service';
 import { InternshipListService } from './internship-list/internship-list.service';
 import { InternshipNewComponent } from './internship-new/internship-new.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+/* Routings */
+const appRoutes: Routes = [
+   { path: '', component: HomeComponent},
+   { path: 'internship-list', component: InternshipListComponent },
+   { path: 'internship-new', component: InternshipNewComponent} 
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InternshipListComponent,
     InternshipDetailsComponent,
-    InternshipNewComponent
+    InternshipNewComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [InternshipService, InternshipListService],
   bootstrap: [AppComponent]
