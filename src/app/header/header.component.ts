be50f10b97ab151aa82dc8d7fc0beb10bba6d136
jeffyjo <jeff_1992_1992@hotmail.com>
2017-05-30 13:34:27 +0200

    Collapsable navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../style/scss/header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  toggleState = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toogleState(){
+    console.log(this.toggleState);
+    if(this.toggleState){
+      this.toggleState = false;
+    } else {
+      this.toggleState = true;
+    }
   }
 
 }
